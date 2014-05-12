1a9e2c1176bd9c010ee4fd4eb19ac9b6839be650
Greg Borenstein <greg.borenstein@gmail.com>
2014-05-12 18:11:17 -0400

    first commit

---
+++ src/ofxPCA.cpp
@@ -0,0 +1,15 @@
+//
+//  ofxPCA.cpp
+//  emptyExample
+//
+//  Created by Greg Borenstein on 5/12/14.
+//
+//
+
+#include "ofxPCA.h"
+
+ofxPCA::ofxPCA(){
+}
+
+ofxPCA::~ofxPCA(){
+}
---
+++ src/ofxPCA.h
@@ -0,0 +1,23 @@
+//
+//  ofxPCA.h
+//  emptyExample
+//
+//  Created by Greg Borenstein on 5/12/14.
+//
+//
+
+#ifndef __emptyExample__ofxPCA__
+#define __emptyExample__ofxPCA__
+
+#include "../libs/Eigen/include/Eigen/Core"
+//#include "Eigen/Eigen"
+
+
+class ofxPCA{
+public:
+    ofxPCA();
+    ~ofxPCA();
+};
+
+
+#endif /* defined(__emptyExample__ofxPCA__) */
